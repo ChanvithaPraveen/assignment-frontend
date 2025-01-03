53b9444d525b318af5c413f186280cc09d0d068f
Chanvitha Praveen <chanvithapraween@gmail.com>
2025-01-03 23:41:41 +0530

    Repo initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import AppRoutes from './routes/AppRoutes'
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+
+  return (
+    <>
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    </>
+  )
+}
+
+export default App
